51feccac59abbea5967256a6e66b30414296928a
Erick Maia <erickmaiatmariano@gmail.com>
2023-03-01 01:52:19 -0300

    Listening to player input

--- FirstScript/Scenes/GodotSprite.gd
+++ FirstScript/Scenes/GodotSprite.gd
@@ -4,6 +4,7 @@ extends Sprite
 # Declare member variables here. Examples:
 var speed = 400
 var angular_speed = PI
+var growing_speed = 0.5
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,8 +14,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation += angular_speed * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+		
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
 	
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
 
 	position += velocity * delta
